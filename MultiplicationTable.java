e424816a79caeead3072c50c2714941f546807eb
Bijal T Benny <145442911+bijalbenny@users.noreply.github.com>
2023-10-28 14:26:12 +0530

    Multiplication Table of a number

---
+++ MultiplicationTable.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class MultiplicationTable {
+
+	public static void main(String[] args) {
+		int number;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number");
+		number=sc.nextInt();
+		for(int i=1;i<=10;i++) {
+			System.out.println(+i+"*"+number+"="+(i*number));
+		}
+
+	}
+
+}
