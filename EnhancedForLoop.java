21a84a972b219ac52dc2fffaff0cc536231a9236
Bijal T Benny <145442911+bijalbenny@users.noreply.github.com>
2023-10-28 14:27:41 +0530

    Enhanced FOR LOOP demo

---
+++ EnhancedForLoop.java
@@ -0,0 +1,13 @@
+
+public class Revision {
+
+	public static void main(String[] args) {
+		String[] fruits = {"Apple","Orange","Strawberry","Grapes"};
+		for(String FRUITS : fruits) {
+			System.out.println(FRUITS);
+		}
+		
+ 
+	}
+
+}
