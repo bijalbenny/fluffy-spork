fe6e147a796ecb2a653eaf4dc616f839eadc7a7d
Bijal T Benny <145442911+bijalbenny@users.noreply.github.com>
2023-11-01 15:07:09 +0530

    CopyArray.java

--- CopyArray.java
+++ CopyArray.java
@@ -12,11 +12,12 @@ public class CopyArray {
         char [] copy1 =new char[10];   
         System.arraycopy(copyFrom, 0, copy1, 0, 3);
         System.out.println(new String(copy1));
+		//find length
         int len=copyFrom.length;
-        
         System.out.println("Length is "+copyFrom.length);
-       /**********************************====*************************************/
-        //Another method
+		}
+	}
+/* Another method
 		int[] array1 = {1, 2, 3};
 		int[] array2 = {4, 5, 6};
 
@@ -27,8 +28,5 @@ public class CopyArray {
 		System.arraycopy(array1, 0, result1, 0, aLen);
 		System.arraycopy(array2, 0, result1, aLen, bLen);
         System.out.println("Another type :");
-		System.out.println(Arrays.toString(result1));
-  
-		}
-	}
+		System.out.println(Arrays.toString(result1));*/
 
