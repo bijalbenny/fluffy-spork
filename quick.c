639e7ab1a3d35d59fab0f1fafbbfdc83d5179934
Bijal T Benny <145442911+bijalbenny@users.noreply.github.com>
2023-11-28 11:28:46 +0530

    Create quick.c

---
+++ quick.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+void swap(int arr[], int i, int j) {
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+}
+
+int partition(int arr[], int low, int high) {
+    int pivot = arr[high];
+    int i = low - 1;
+
+    for (int j = low; j < high; j++) {
+        if (arr[j] <= pivot) {
+            i++;
+            swap(arr, i, j);
+        }
+    }
+
+    swap(arr, i + 1, high);
+    return i + 1;
+}
+
+void quickSort(int arr[], int low, int high) {
+    if (low < high) {
+        int pi = partition(arr, low, high);
+
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int arr_size = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Given array: \n");
+    printArray(arr, arr_size);
+
+    quickSort(arr, 0, arr_size - 1);
+
+    printf("Sorted array: \n");
+    printArray(arr, arr_size);
+
+    return 0;
+}
