fe0a4b7b9d3fce7db4d0579f33bd261237cf83ef
Bijal T Benny <145442911+bijalbenny@users.noreply.github.com>
2023-12-18 08:24:45 +0530

    expression conversion.c

--- expression conversion.c
+++ expression conversion.c
@@ -58,3 +58,84 @@ int priority(char element) {
     else
         return 0;
 }
+
+
+
+
+
+
+
+
+/*
+    
+This C program converts an 
+infix expression to its corresponding 
+postfix expression using a stack-based 
+    approach. Here's an explanation of 
+    the code:
+
+Stack Implementation:
+
+int top = -1; char stack[20];
+declares a stack with a maximum
+capacity of 20 elements and 
+initializes the top of the stack to -1.
+The stack is used to temporarily store
+operators while processing the 
+infix expression.
+Function Declarations:
+
+void push(char); and int pop();
+are functions to push and pop elements 
+onto/from the stack.
+int priority(char); 
+is a function to determine the 
+priority of operators.
+Main Function:
+
+push('('); initializes the stack with 
+an opening parenthesis to handle edge 
+cases during the conversion.
+char infix[20]; scanf("%s", infix); 
+reads the infix expression from the user.
+Conversion Loop:
+
+The program then iterates through each
+character in the infix expression
+using a for loop.
+If the character is an alphanumeric
+operand, it is directly printed.
+If the character is an opening 
+parenthesis '(', it is pushed onto the stack.
+If the character is a closing parenthesis ')',
+ elements are popped from the stack until an
+opening parenthesis is encountered.
+For operators, it compares the priority of 
+the current operator with the one
+on the top of the stack and pops
+elements accordingly.
+The current operator is then pushed
+onto the stack.
+Finalization:
+
+After processing the entire infix 
+expression, the program pops any 
+remaining elements from the stack
+and prints them.
+Push and Pop Functions:
+
+void push(char pushElement) increments 
+the top index and adds the specified 
+element to the stack.
+int pop() retrieves the top element
+from the stack, prints it 
+(except for opening parenthesis), and
+decrements the top index.
+Priority Function:
+
+int priority(char element) returns 
+the priority of an operator based on a
+predefined hierarchy (exponentiation has
+the highest priority, followed by
+multiplication and division, and then 
+addition and subtraction).
