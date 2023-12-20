d418ce0443e7fd7a08b7637719c4d7d1faf6622f
Bijal T Benny <145442911+bijalbenny@users.noreply.github.com>
2023-12-20 07:27:14 +0530

    Create Fibonacci.java

---
+++ Fibonacci.java
@@ -0,0 +1,29 @@
+package Javalab;
+import java.util.Scanner;
+public class ConstructorOverloading {
+	public static void main(String[] args) {
+		Fibonacci fibonacci = new Fibonacci();
+		fibonacci.generateFibonacci();
+	}
+}
+class Fibonacci {
+	int firstNumber;
+	int secondNumber;
+	int thirdNumber;
+	Fibonacci(){
+		firstNumber = 0;
+		secondNumber = 1;
+	}
+public void generateFibonacci(){
+	System.out.println("Enter the limit:");
+	Scanner sc = new Scanner(System.in);
+	int limit = sc.nextInt();
+	for(int i=0;i<limit;i++) {
+		thirdNumber = firstNumber + secondNumber;
+		System.out.println(thirdNumber+"\t");
+		firstNumber = secondNumber;
+		secondNumber = thirdNumber;
+	}
+}
+}
+	
